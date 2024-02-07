3c391b71454e2fcfba5951e2f2fdf3e8d718787f
Swatantrajaiswal <swatantra1950@gmail.com>
2024-02-07 12:25:23 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/**
+ * Hello
+ */
+class Hello
+
+{
+
+public static void main(String[] args) 
+{
+    int num1 = 6;
+    //int num2 = 1035;
+    //float result = (num2/ num1);
+
+    int result = num1++;
+
+    System.out.println(result);
+
+}
+}
