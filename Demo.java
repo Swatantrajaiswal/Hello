bcd661990e2f5fe4b010b1c5a6e600fcac86967a
Swatantrajaiswal <swatantra1950@gmail.com>
2024-02-07 18:21:50 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,23 @@
+public class Demo
+{
+
+    public static void main(String[] args) {
+        int i = 4;
+
+        while(i<=4);
+        {
+            System.out.println("Hello" + i);
+            int j =1;
+            while (j<=3){
+                System.out.println("Hi" + j);
+                j++;
+                
+            }
+
+            i++;
+
+        }
+        System.out.println("bye" + i);
+ 
+    }
+}
